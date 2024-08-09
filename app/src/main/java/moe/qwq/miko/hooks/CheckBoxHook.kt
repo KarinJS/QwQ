898a38743f414ed18a9507d0235a9f3847f9b9df
owo233 <ver@vip.qq.com>
2024-08-10 02:03:37 +0800

    fixed: active checkbox

--- app/src/main/java/moe/qwq/miko/hooks/CheckBoxHook.kt
+++ app/src/main/java/moe/qwq/miko/hooks/CheckBoxHook.kt
@@ -6,23 +6,24 @@ import de.robv.android.xposed.XC_MethodHook
 import de.robv.android.xposed.XposedBridge
 import moe.fuqiuluo.processor.HookAction
 import moe.qwq.miko.actions.IAction
-import moe.qwq.miko.ext.beforeHook
-import moe.qwq.miko.ext.hookMethod
 import moe.qwq.miko.internals.setting.QwQSetting
 
 @HookAction(desc = "默认勾选复选框")
 class CheckBoxHook: IAction {
     override fun onRun(ctx: Context) {
+        val loginContext = listOf(
+            "com.tencent.mobileqq.activity.LoginActivity",
+            "com.tencent.mobileqq.activity.LoginPublicFragmentActivity"
+        )
         XposedBridge.hookAllConstructors(CheckBox::class.java, object : XC_MethodHook() {
             override fun afterHookedMethod(param: MethodHookParam) {
-                val checkBox = param.thisObject as CheckBox
-                checkBox.post { checkBox.isChecked = true }
+                val context = param.args[0] as Context
+                if (loginContext.contains(context.javaClass.name)) {
+                    val checkBox = param.thisObject as CheckBox
+                    checkBox.post { checkBox.isChecked = true }
+                }
             }
         })
-        val hook = beforeHook {
-            it.args[0] = true
-        }
-        CheckBox::class.java.hookMethod("setChecked", hook)
     }
 
     override val name: String = QwQSetting.CHECK_BOX_HOOK
